45b5a31bb69b072b24c76a79ee3428853dbedd88
Robson Lucas Silveira Mello <robson.mello3@etec.sp.gov.br>
2019-07-10 14:12:49 -0300

    1.1

--- script/mine/Skills.js
+++ script/mine/Skills.js
@@ -39,19 +39,21 @@ function criarSkill(_src, _alt, _title, _prof){
     }
     skills.push(skill);
 }
-skills.push(new Skill("img/php.svg","php","PHP",3,"POO, POE, MVC, emails, manipulação de cookies e sessões, conexão ao banco de dados(PDO), manipução de recepção de arquivos, consumo de webservices"));
+skills.push(new Skill("img/php.svg","php","PHP",3,"POO, POE, MVC, emails, manipulação de cookies e sessões, conexão ao banco de dados(PDO), manipução de recepção de arquivos, consumo e criação de webservices JSON"));
+skills.push(new Skill("img/node.svg","node","Node.JS",3,"POO, POE, padrão MVC, conexão ao banco de dados(MySql), manipução de recepção de arquivos, consumo de webservices, Criação de API's,(Express, Nodemon, Ejs, body-parser), NPM"));
 skills.push(new Skill("img/aspnet.png","aspnet","Asp.net",4,"POO, POE, MVC, emails, manipulação de sessões, conexão ao banco de dados(MySql, SqlServer), Criação e uso de WebServices, cshtml, razer, NUGET, consumo de webservice e apis, geração de relátorios, manipulação e criação de arquivos"));
-skills.push(new Skill("img/mysql.svg","mysql","MySql Programmer",4,"Selects(Simples, joins, subselects), funções, uso de Workbench, criação do banco de dados(relação PK, FK, UQ, objeto de resolução), passagem de toda regras de negócio para o banco, criação de views, procedures, functions, cursores, regex, MD5"));
+skills.push(new Skill("img/mysql.svg","mysql","MySql Programmer",4,"Selects(Simples, joins, subselects), uso de Workbench, criação do banco de dados(relação PK, FK, UQ, objeto de resolução), passagem de toda regras de negócio para o banco, criação de views, procedures, functions, cursores, regex, MD5"));
+skills.push(new Skill("img/sqlite.png","sqlite3","Sqlite 3",3,"Selects(Simples, joins, subselects), criação do banco de dados(relação PK, FK, UQ, objeto de resolução), passagem de toda regras de negócio para o banco"));
 skills.push(new Skill("img/html5.svg", "Html5", "Html5", 3,"Semântica(Atributos, Data's), Seletores(tag, class, id), Iframe, web integrada(Css, JavaScript, Php/Asp.net)"));
-skills.push(new Skill("img/css-3.svg","css","Css 3",2,"Seletores(tag, class, id); Animação(Transição), Filtros, eventos(hover, active, visited, etc), Framework(Bootstrap 4.1)"));
-skills.push(new Skill("img/js.png","js","Javascript",3,"POO, POE, manipulação de eventos, DOM, Animação, manipução de cookies, Framework/biblioteca(Bootstrap 4.1, Jquery, SweetAlert), consumo de webservices, básico de ecmascript"));
-skills.push(new Skill("img/csharp.svg","csharp","C#",4,"POO, POE, MVC, emails, conexão ao banco de dados(MySql, SqlServer), NUGET, consumo de webservice e apis, geração de relátorios, manipulação e criação de arquivos"));
+skills.push(new Skill("img/css-3.svg","css","Css 3",3,"Seletores(tag, class, id); Animação(Transição), Filtros, eventos(hover, active, visited, etc), Framework(Bootstrap 4.1), manipulação de filhos, gradientes, @media"));
+skills.push(new Skill("img/js.png","js","Javascript",3,"POO, POE, manipulação de eventos, DOM, Animação, manipução de cookies, Framework/biblioteca(Bootstrap 4.1, Jquery, SweetAlert), consumo de webservices, básico de ecmascript, Callbacks"));
+skills.push(new Skill("img/csharp.svg","csharp","C#",4,"POO, POE, MVC, emails, conexão ao banco de dados(MySql, SqlServer), NUGET, consumo de webservice e apis, geração de relátorios, manipulação e criação de arquivos, consumo de dados via bluetooth"));
 skills.push(new Skill("img/java.svg","java","Java",3,"POO, POE, MVC, emails, conexão ao banco de dados(MySql), geração de relátorios"));
-skills.push(new Skill("img/python.png","py","Python",3,"POO, Ponteiros, manipulação de strings, básico de Django(formato MTV), uso de banco de dados mysql, uso de bibliotecas"));
+skills.push(new Skill("img/python.png","py","Python",3,"POO, Ponteiros, manipulação de strings, básico de Django(formato MTV), uso de banco de dados mysql, uso de bibliotecas, envio de emails"));
 skills.push(new Skill("img/c.svg","c++","C++",3,"POO, POE, Ponteiros, manipulação de strings, manipulação e criação de arquivos"));
 skills.push(new Skill("img/arduino.png","arduino","Arduino",3,"POE, POO, Manipulação e recepção das portas digitais e analogicas, uso e monitoramento de Serial, uso de bibliotecas"));
 skills.push(new Skill("img/eclipse.png","eclipse","Eclipse",2,"Uso da linguagem Java(POO, POE), programação para Android, conexão ao banco de dados(sqlLite)"));
-skills.push(new Skill("img/unity.png","unity","Unity",3,"Uso da linguagem C#(POO, POE), desenvolvimento 2D e 3D, manipução de assets, criação de animação, scriptable objects, bibliotecas(PlayerPrefsX, Dragable), Prefabs, Sistema RPG(exp, combate, história), conexão ao banco de dados(MySql), consumo de webservices"));
+skills.push(new Skill("img/unity.png","unity","Unity",4,"Uso da linguagem C#(POO, POE), desenvolvimento 2D e 3D, manipução de assets, criação de animação, scriptable objects, bibliotecas(PlayerPrefsX, Dragable), Prefabs, Sistema RPG(exp, combate, história), conexão ao banco de dados(MySql), consumo de webservices, consumo de dados via bluetooth"));
 skills.push(new Skill("img/jQuery-2.png","jQuery","jQuery",3,"Manipução de eventos, Manipução e recepção de dados, DOM"));
 skills.push(new Skill("img/AJAX_logo_by_gengns.svg.png","AJAX","Ajax",2,"Requição de dados via protocolo, manipução dos dados recebidos"));
 skills.push(new Skill("img/word.svg","word","Word",3,"Formatação em regras ABNT, uso de funções"));
